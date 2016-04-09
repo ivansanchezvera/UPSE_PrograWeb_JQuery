715da4272b651033f3e3ec7311677f1f1d03ead5
Ivan <ivansanchez_1988@hotmail.com>
2016-04-09 14:03:03 -0500

    JQuery Intro: Llamando a JQuery y usando show y hide

---
+++ js/app.js
@@ -0,0 +1,7 @@
+//Esconder el mensaje
+jQuery(".advertencia").hide();
+
+//mostrar el mensaje lentamente usamos atajo $
+$(".advertencia").show("slow");
+
+jQuery(".advertencia").hide().show("slow");
