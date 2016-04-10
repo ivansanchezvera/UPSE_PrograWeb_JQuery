c02fb24d14c64b8b6fd070666b3259ac514a1f94
Ivan <ivansanchez_1988@hotmail.com>
2016-04-09 19:42:11 -0500

    Miniproyecto Spoilers:

--- js/app.js
+++ js/app.js
@@ -1,6 +1,36 @@
+/*
+4 ps
+Problema: No arruinar Breaking bad a los que no lo han visto.
+Solucion: Usar hide y mostrar el spolier solo 
+a los usuarios que quieran
+
+Planear:
+1. Esconder spoiler
+2. agregar boton
+3. cuando se presiona el boton
+	3.1 Mostrar Spoiler
+	3.2 Deshacerse del boton
+*/
+
+//1. Esconder spoiler
+$(".spoiler span").hide();
+
+//2. Agregar boton
+$(".spoiler").append("<button>Mostrar Spoiler!</button>");
+
+//3 Cuando se presiona el boton
+//Agregar un handler al evento clic
+$(".spoiler button").click(function(){
+	//3.1 Mostrar Spoiler
+	$(".spoiler span").show();
+
+	//3.2 Deshacerse del boton
+	$(this).remove();
+})
+
 function enviarAdvertencia()
 {
-	jQuery(".advertencia").hide().show("slow");
+	jQuery(".spoiler").hide().show("slow");
 }
 
 //Le pasamos al handler la funcion sin parentesis
